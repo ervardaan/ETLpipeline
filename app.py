fae9235826cdc101df27b3fc5f8e70a0d83cab02
ervardaan <vardaan2807@gmail.com>
2024-11-16 12:52:04 -0600

    feature derivation completed

--- app.py
+++ app.py
@@ -1,49 +1,60 @@
 import csv
 import pandas as pd
+from collections import Counter
+import statistics
 def etl(fileName):
     # Load CSV files
     # Process files to derive features
     # Upload processed data into a database
     with open(fileName,newline='') as experimentFile:
         fileReader=csv.reader(experimentFile,delimiter=',')
-        usersExperiments=pd.read_csv(fileName)
-        userDict={}
-        experimentAvg={}
+        userDict={}#create a user dictionary to collect user-number of experiments data
+        userRunTime={}
+        userCommonCompound={}
         i=0
         for row in fileReader:
             if i<1:
                 i=i+1
                 continue
-            experimentId=int(row[0].strip('\t'))
-            userId=int(row[1].strip('\t'))
-            compounds=row[2].strip('\t')
-            runTime=int(row[3].strip('\t'))
+            experimentId=int(row[0].strip('\t'))#get experiment id
+            userId=int(row[1].strip('\t'))# get userid
+            compounds=row[2].strip('\t')# get compounds list
+            runTime=int(row[3].strip('\t'))# get runtime
+            compoundsCount={}#not needed
             compoundList=[]
-            compoundsCount={}
             for compound in compounds.split(';'):
                 compoundSeen=int(compound)
+                compoundList.append(compoundSeen)
                 if compoundSeen in compoundsCount:
                     compoundsCount[compoundSeen]=compoundsCount[compoundSeen]+1
                 else:
                     compoundsCount[compoundSeen]=1
             if userId in userDict:
                 userDict[userId]=userDict[userId]+1
+                userRunTime[userId]=userRunTime[userId]+runTime
+                currentList=userCommonCompound[userId]
+                for c in compoundList:
+                    currentList.append(c)
+                userCommonCompound[userId]=currentList
             else:
                 userDict[userId]=1
-        exp_id=usersExperiments.columns.tolist()[0].strip('\t')
-        print(df)
-        print(userDict)
-
-        
-    pass
-
-
+                userRunTime[userId]=runTime
+                userCommonCompound[userId]=compoundList
+        featuresList=[]
+        featuresList.append(userDict)
+        #countAverageExperiments=sum(userDict.values())/len(userDict)
+        userAvgRuntime={}
+        for (key,value) in userRunTime.items():
+            userAvgRuntime[key]=value/userDict[key]
+        for (key,listValue) in userCommonCompound.items():
+                mode=statistics.mode(listValue)
+                userCommonCompound[key]=mode
+        return(featuresList,userAvgRuntime,userCommonCompound)
 # Your API that can be called to trigger your ETL process
 def trigger_etl():
     # Trigger your ETL process here
-    etl()
+    etl("C:/Users/varda/OneDrive/Documents/COLLEGE COURSEWORK/ETLpipeline/data/user_experiments.csv")
     return {"message": "ETL process started"}, 200
 if __name__=="__main__":
-    etl("data/user_experiments.csv")
-
+    trigger_etl()
 
