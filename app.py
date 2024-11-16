f8f9449282e390d3dfbac1fc65a6f35efcc09584
ervardaan <vardaan2807@gmail.com>
2024-11-16 14:38:56 -0600

    initializing empty database

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import csv
 import pandas as pd
 from collections import Counter
 import statistics
-def etl(fileName):
+def etl(fileName,outputFileName):
     # Load CSV files
     # Process files to derive features
     # Upload processed data into a database
@@ -49,11 +49,16 @@ def etl(fileName):
         for (key,listValue) in userCommonCompound.items():
                 mode=statistics.mode(listValue)
                 userCommonCompound[key]=mode
+        # with open(outputFileName,'w',newline='') as outputFile:
+        #     fileWriter=csv.writer(outputFile,delimiter=',')
+        #     print("hello")
+        #     for row in fileReader:
+        #         print(row)
         return(featuresList,userAvgRuntime,userCommonCompound)
 # Your API that can be called to trigger your ETL process
 def trigger_etl():
     # Trigger your ETL process here
-    etl("C:/Users/varda/OneDrive/Documents/COLLEGE COURSEWORK/ETLpipeline/data/user_experiments.csv")
+    etl("C:/Users/varda/OneDrive/Documents/COLLEGE COURSEWORK/ETLpipeline/data/user_experiments.csv","C:/Users/varda/OneDrive/Documents/COLLEGE COURSEWORK/ETLpipeline/data/users.csv")
     return {"message": "ETL process started"}, 200
 if __name__=="__main__":
     trigger_etl()
